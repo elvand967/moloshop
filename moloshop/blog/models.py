6026b8005c219b55f9f37a17b20440a8508dccd9
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-07-02 10:12:27 +0300

    migrate 0001

--- moloshop/blog/models.py
+++ moloshop/blog/models.py
@@ -1,3 +1,11 @@
+# D:\Python\django\elvand\moloshop\blog\models.py
+
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
--- moloshop/moloshop/urls.py
+++ moloshop/moloshop/urls.py
@@ -1,14 +1,20 @@
 # D:\Python\django\elvand\moloshop\moloshop\urls.py
-
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from blog.views import *
+from moloshop import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
 
+
+# процес необходим для работы в отладочном режиме
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = pageNotFound
+
