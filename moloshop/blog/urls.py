24bfd3a9b727fab30f926a907d854f33ff3639fd
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-07-09 16:14:37 +0300

    user

--- moloshop/blog/urls.py
+++ moloshop/blog/urls.py
@@ -5,16 +5,16 @@ from django.urls import path, re_path
 
 from blog.views import *
 
+
+
+
 urlpatterns = [
-    # path('', index, name='home'),
     path('', BlogHome.as_view(), name='home'),
     path('about/', about, name='about'),
-    # path('addpage/', addpage, name='add_page'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    # path('post/<slug:post_slug>/', show_post, name='post'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    # path('category/<int:cat_id>/', show_category, name='category'),
     path('category/<slug:cat_slug>/', BlogCategory.as_view(), name='category'),
 ]
--- moloshop/blog/views.py
+++ moloshop/blog/views.py
@@ -1,11 +1,14 @@
 # D:\Python\django\elvand\moloshop\blog\views.py
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.forms import forms
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 
-from .forms import AddPostForm
+from .forms import AddPostForm, RegisterUserForm
 from .models import *
 
 from django.views.generic import ListView, DetailView, CreateView
@@ -67,6 +70,19 @@ class BlogCategory(DataMixin, ListView):
                                       cat_selected=context['posts'][0].cat_id)
         return dict(list(context.items()) + list(c_def.items()))
 
+
+
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 
